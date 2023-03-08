85fe3f5fc650deac3ea948da2ad63df24412b24f
Gaven <93108135+GavenZh@users.noreply.github.com>
2023-03-08 23:07:57 +0800

    Add files via upload

---
+++ Project1/张佳文_2021214037_python上机1/2.py
@@ -0,0 +1,15 @@
+x=int(input('请输入一个正整数:'))
+if x<=0 :
+    print('输入错误')
+else :
+    flag = 2
+    print(x,"的质因数为x=",sep='',end='')
+    while flag <= x:
+        if x % flag == 0 and (not flag==x):
+          print(flag,end='*')
+          x = x / flag
+        elif flag == x :
+          print(flag, end=' ')
+          break
+        else:
+          flag+= 1
---
+++ Project1/张佳文_2021214037_python上机1/3.py
@@ -0,0 +1,8 @@
+n,flag,s=10,1,0
+for i in range(1,n+1):
+    flag=flag*i
+    s=s+flag
+    print(f'{flag}+',sep='',end='')
+print(f'\b={s}',sep='',end='')
+
+
